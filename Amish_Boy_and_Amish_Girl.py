97de296b9e97cbf222e46739d1752ffb1bdc7ac2
Ryan-J-Bender <116182464+Ryan-J-Bender@users.noreply.github.com>
2022-10-20 11:36:25 -0400

    Added Message

---
+++ Amish_Boy_and_Amish_Girl.py
@@ -0,0 +1,17 @@
+# CSCN 345 (003) Python Game by Isaac Scott and Ryan Bender
+
+import pygame
+
+# initialize
+pygame.init()
+
+# game loop
+
+    # input
+
+    # update
+
+    # draw
+
+# quit
+pygame.quit()
