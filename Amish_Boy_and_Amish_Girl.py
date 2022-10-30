85e0dfbf9da5e5c5573677209eaa2af57d30d87f
Ryan-J-Bender <116182464+Ryan-J-Bender@users.noreply.github.com>
2022-10-30 15:24:47 -0400

    Add sprites, collisions, platforms, background

--- Amish_Boy_and_Amish_Girl.py
+++ Amish_Boy_and_Amish_Girl.py
@@ -14,37 +14,44 @@ FRIC = -0.12
 FPS = 60
 
 bg = pygame.image.load('bgimgFade.png') # Loads background image into bg variable
-
-# boy_sprite_sheet = pygame.image.load('Boy Sprite Sheet.png').convert_alpha()
-
-# girl_sprite_sheet = pygame.image.load('Girl Sprite Sheet.png').convert_alpha()
  
 FramePerSec = pygame.time.Clock()
  
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Amish Boy and Amish Girl")
 
+boy_sprite_sheet = pygame.image.load('Boy.png').convert_alpha()
+girl_sprite_sheet = pygame.image.load('Girl.png').convert_alpha()
+
+# def getImage(sheet, width, height):
+#     image = pygame.Surface((width, height)).convert_alpha
+
+#     return image
+
 #class for player
 class Player1(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((128,255,40))
+        self.surf = boy_sprite_sheet
         self.rect = self.surf.get_rect()
 
-        self.pos = vec((10, 385))
+        self.pos = vec((100, HEIGHT - 11))
         self.vel = vec(0,0)
     
     #colision update
     def update1(self):
         platforms = pygame.sprite.Group()
         platforms.add(PT1)
+        platforms.add(PT2)
 
         hits = pygame.sprite.spritecollide(P1 ,platforms, False)
         if P1.vel.y > 0:        
             if hits:
                 self.vel.y = 0
                 self.pos.y = hits[0].rect.top + 1
+        if P1.vel.y < 0:        
+            if hits:
+                self.vel.y = 15       
                 
     #movement function
     def move1(self):
@@ -69,36 +76,43 @@ class Player1(pygame.sprite.Sprite):
     def jump1(self):
         platforms = pygame.sprite.Group()
         platforms.add(PT1)
+        platforms.add(PT2)
         hits1 = pygame.sprite.spritecollide(self, platforms, False)
         if hits1:
             self.vel.y = -15
             
 class Player2(pygame.sprite.Sprite):
+    
     def __init__(self):
         super().__init__() 
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((255,50,40))
+       # self.surf = getImage(girl_sprite_sheet, 64, 70)
+        self.surf = girl_sprite_sheet
+
         self.rect = self.surf.get_rect()
 
-        self.pos = vec((10, 385))
+        self.pos = vec((WIDTH - 100, HEIGHT - 11))
         self.vel = vec(0,0)
     
     #colision update   
     def update2(self):
         platforms = pygame.sprite.Group()
         platforms.add(PT1)
+        platforms.add(PT2)
         hits = pygame.sprite.spritecollide(P2 ,platforms, False)
         if P2.vel.y > 0:        
             if hits:
                 self.vel.y = 0
                 self.pos.y = hits[0].rect.top + 1
+        if P2.vel.y < 0:        
+            if hits:
+                self.vel.y = 15   
             
     #movement function        
     def move2(self):
         self.acc = vec(0,0.5)
  
-        pressed_keys = pygame.key.get_pressed()
-            
+        pressed_keys = pygame.key.get_pressed()    
+        
         if pressed_keys[K_a]:
             self.acc.x = -ACC
         if pressed_keys[K_d]:
@@ -116,26 +130,76 @@ class Player2(pygame.sprite.Sprite):
     def jump2(self):
         platforms = pygame.sprite.Group()
         platforms.add(PT1)
+        platforms.add(PT2)
         hits2 = pygame.sprite.spritecollide(self, platforms, False)
         if hits2:
             self.vel.y = -15
         
 #class for platforms 
 class platform(pygame.sprite.Sprite):
+    def __init__(self, sizex, sizey, posx, posy):
+        super().__init__()
+        self.surf = pygame.Surface((sizex, sizey))
+        self.surf.fill((255,255,12))
+        self.rect = self.surf.get_rect(center = (posx, posy))
+        
+class goal(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((WIDTH, 20))
-        self.surf.fill((255,0,0))
-        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+        self.surf = pygame.Surface((20, 20))
+        self.surf.fill((255,255,12))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 40))
+ 
+class go(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, HEIGHT))
+        self.surf.fill((0,0,0))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT/2))
         
-PT1 = platform()
+class w(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, HEIGHT))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT/2))
+        
+def gameover():
+    g = go()
+    all_sprites.add(g)  
+    
+def win():
+    won = w()
+    all_sprites.add(won)  
+        
+#player colision detection        
+def colision(boy, girl):
+    players = pygame.sprite.Group()
+    players.add(P2)
+    hitsgo = pygame.sprite.spritecollide(boy, players, False)
+    if hitsgo:
+        gameover()
+    players.add(P1)
+    hitsw = pygame.sprite.spritecollide(obj, players, False)
+    if hitsw:
+       win()
+    
+    
+
+        
+        
+PT1 = platform(WIDTH, 20, WIDTH/2, HEIGHT-10)
+PT2 = platform(20, HEIGHT, WIDTH/2, HEIGHT/2)
 P1 = Player1()
 P2 = Player2()
+obj = goal()
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(PT1)
+all_sprites.add(PT2)
 all_sprites.add(P1)
 all_sprites.add(P2)
+#all_sprites.add(obj)
 
 
 #while loop for game run
@@ -162,5 +226,6 @@ while True:
     for entity in all_sprites:
         displaysurface.blit(entity.surf, entity.rect)
  
+    colision(P1, P2)
     pygame.display.update()
     FramePerSec.tick(FPS)
